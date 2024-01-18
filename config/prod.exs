b15e92fbdbfcad84ea1deeaf73c355e6fd51d4a9
Sarah Tani <sarahtani2022@u.northwestern.edu>
2024-01-17 18:22:05 -0600

    feat: build

--- config/prod.exs
+++ config/prod.exs
@@ -1,6 +1,7 @@
 import Config
 
 config :drop7_live, Drop7LiveWeb.Endpoint,
+  url: [host: System.get_env("RENDER_EXTERNAL_HOSTNAME") || "localhost", port: 80],
   cache_static_manifest: "priv/static/cache_manifest.json"
 
 # Do not print debug messages in production
