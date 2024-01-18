cb5236dd249e6f093030490becc40bf421e70737
Sarah Tani <sarahtani2022@u.northwestern.edu>
2024-01-17 18:13:59 -0600

    feat: how to play

--- lib/drop7_live_web/game_live.ex
+++ lib/drop7_live_web/game_live.ex
@@ -2,85 +2,100 @@ defmodule Drop7Web.GameLive do
   use Drop7LiveWeb, :live_view
 
   alias Drop7.Turn
+  alias Phoenix.LiveView.JS
 
   # Time in ms that schedules the game loop
   @tick 500
 
   def render(assigns) do
     ~H"""
-    <div class="score">
-      <%= Number.Delimit.number_to_delimited(trunc(@game_state.score)) %>
-    </div>
+    <div class="game">
+      <div class="instructions">
+        <h2 phx-click={JS.toggle(to: "#instructions")}>How to Play</h2>
+        <p id="instructions">Drop tiles to build up rows and columns.
 
-    <div class="turn-tracker">
-      <div class={"turn-#{@game_state.turn_count > 4}"} />
-      <div class={"turn-#{@game_state.turn_count > 3}"} />
-      <div class={"turn-#{@game_state.turn_count > 2}"} />
-      <div class={"turn-#{@game_state.turn_count > 1}"} />
-      <div class={"turn-#{@game_state.turn_count > 0}"} />
-    </div>
+        When a tile is in a row or column of length equal to the tile's value, it will pop!
 
-    <div class="game-container">
-      <div class="animating-modal" hidden={not @animating} />
-      <div class="modal" hidden={not @game_state.game_over}>
-        <div class="modal-content">
-          <h1>Game Over</h1>
+        New "egg" tiles will appear every five turns. Pop tiles adjacent to the eggs to reveal their values.
 
-          <button phx-click="start" class="btn">
-            Play Again
-          </button>
-        </div>
+        Play till the board overflows. Good luck!
+        </p>
       </div>
 
-      <div class="next-tile">
-        <div class="slot">
-          <div class={"tile tile-" <> to_string(@game_state.next_tile.value)} hidden={@animating}>
-            <%= @game_state.next_tile.value %>
+      <div class="score">
+        <%= Number.Delimit.number_to_delimited(trunc(@game_state.score)) %>
+      </div>
+
+      <div class="turn-tracker">
+        <div class={"turn-#{@game_state.turn_count > 4}"} />
+        <div class={"turn-#{@game_state.turn_count > 3}"} />
+        <div class={"turn-#{@game_state.turn_count > 2}"} />
+        <div class={"turn-#{@game_state.turn_count > 1}"} />
+        <div class={"turn-#{@game_state.turn_count > 0}"} />
+      </div>
+
+      <div class="game-container">
+        <div class="animating-modal" hidden={not @animating} />
+        <div class="modal" hidden={not @game_state.game_over}>
+          <div class="modal-content">
+            <h1>Game Over</h1>
+
+            <button phx-click="start" class="btn">
+              Play Again
+            </button>
           </div>
         </div>
-      </div>
 
-      <div class="game-board">
-        <div :for={{row, index} <- Enum.with_index(@game_state.game_objects)}>
-          <div :for={tile_or_slot <- row} phx-click={index}>
-            <%= if tile_or_slot.type == :empty do %>
-              <div class="slot empty-slot">
-                <div class="tile tile-empty" />
-              </div>
-            <% end %>
+        <div class="next-tile">
+          <div class="slot">
+            <div class={"tile tile-" <> to_string(@game_state.next_tile.value)} hidden={@animating}>
+              <%= @game_state.next_tile.value %>
+            </div>
+          </div>
+        </div>
 
-            <%= if Map.get(tile_or_slot, :type) == :overflow do %>
-              <%= if Map.get(tile_or_slot, :value) do %>
-                <div class="slot game-over-slot">
-                  <div class={"tile tile-" <> to_string(tile_or_slot.value)}>
-                    <%= tile_or_slot.value %>
-                  </div>
-                </div>
-              <% else %>
-                <div class="overflow-slot">
+        <div class="game-board">
+          <div :for={{row, index} <- Enum.with_index(@game_state.game_objects)}>
+            <div :for={tile_or_slot <- row} phx-click={index}>
+              <%= if tile_or_slot.type == :empty do %>
+                <div class="slot empty-slot">
                   <div class="tile tile-empty" />
                 </div>
               <% end %>
-            <% end %>
 
-            <%= if tile_or_slot.type in [:egg, :cracked] do %>
-              <div class="slot tile-slot">
-                <div class={"tile tile-" <> to_string(tile_or_slot.type) <> Map.get(tile_or_slot, :state, "")}>
-                </div>
-              </div>
-            <% end %>
+              <%= if Map.get(tile_or_slot, :type) == :overflow do %>
+                <%= if Map.get(tile_or_slot, :value) do %>
+                  <div class="slot game-over-slot">
+                    <div class={"tile tile-" <> to_string(tile_or_slot.value)}>
+                      <%= tile_or_slot.value %>
+                    </div>
+                  </div>
+                <% else %>
+                  <div class="overflow-slot">
+                    <div class="tile tile-empty" />
+                  </div>
+                <% end %>
+              <% end %>
 
-            <%= if tile_or_slot.type == :tile do %>
-              <div class="slot tile-slot">
-                <div class={"tile tile-" <> to_string(tile_or_slot.value) <> Map.get(tile_or_slot, :state, "")}>
-                  <%= tile_or_slot.value %>
+              <%= if tile_or_slot.type in [:egg, :cracked] do %>
+                <div class="slot tile-slot">
+                  <div class={"tile tile-" <> to_string(tile_or_slot.type) <> Map.get(tile_or_slot, :state, "")}>
+                  </div>
                 </div>
+              <% end %>
 
-                <div class="points" hidden={Map.get(tile_or_slot, :state, "") != " popping"}>
-                  +<%= Number.Delimit.number_to_delimited(Drop7Live.Score.combo(@game_state.combo)) %>
+              <%= if tile_or_slot.type == :tile do %>
+                <div class="slot tile-slot">
+                  <div class={"tile tile-" <> to_string(tile_or_slot.value) <> Map.get(tile_or_slot, :state, "")}>
+                    <%= tile_or_slot.value %>
+                  </div>
+
+                  <div class="points" hidden={Map.get(tile_or_slot, :state, "") != " popping"}>
+                    +<%= Number.Delimit.number_to_delimited(Drop7Live.Score.combo(@game_state.combo)) %>
+                  </div>
                 </div>
-              </div>
-            <% end %>
+              <% end %>
+            </div>
           </div>
         </div>
       </div>
